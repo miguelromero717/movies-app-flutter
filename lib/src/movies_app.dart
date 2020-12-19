e510a8cd0eb8451864eaf079480285663f0325fb
Miguel Angel Romero <miguelromero717@gmail.com>
2020-12-19 10:44:52 -0500

    - Add swiper and http packages - Add home page and config initial route

---
+++ lib/src/movies_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/src/pages/home_page.dart';
+
+class MoviesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movies App',
+      initialRoute: '/',
+      routes: {'/': (BuildContext context) => HomePage()},
+    );
+  }
+}
